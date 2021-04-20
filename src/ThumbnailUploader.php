d1872153994244b2b460f45f4a291f7ed58bdc88
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-04-20 12:19:03 +0200

    throws

--- src/ThumbnailUploader.php
+++ src/ThumbnailUploader.php
@@ -11,6 +11,7 @@ class ThumbnailUploader
 
     /**
      * @throws BadVideoIdException
+     * @throws RuntimeException
      * @throws ThumbnailFeatureNotAvailableException
      */
     public function upload(
